7be7abccc7fbbd88e8c232c3fa6f6de2772ef2c4
65160219 <152172067+65160219@users.noreply.github.com>
2024-04-06 16:27:11 +0700

    update code edit delete search company

---
+++ resources/views/livewire/company-list.blade.php
@@ -0,0 +1,45 @@
+
+<div>
+    @livewireStyles
+    <div class="search-container" style="position:fixed;right:313px;margin-top:-65px;">
+        <input wire:model.live="search" style="padding-left: 10px;" type="text" class="search" name="search_user" placeholder="Search User">
+        <button type="button" class="search-button" name="btn_search_user">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+    </div>
+    <table border="1">
+        <tr>
+            <th>
+                ID
+            </th>
+            <th>
+                Company Name
+            </th>
+            <th>
+                Action
+            </th>
+        </tr>
+        @foreach ($oe_companies as $company)
+            <tr>
+                <td>
+                    <label> {{ $company['company_id'] }} </label>
+                </td>
+                <td>
+                    <label> {{ $company['company_name'] }} </label>
+                </td>
+                <td>
+                    <livewire:company-action :company="$company" :key="$company->id"></livewire:company-action>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+    <span>
+        {{ $oe_companies->appends(request()->input())->links() }}
+    </span>
+    <style>
+        .w-5 {
+            display: none;
+        }
+    </style>
+    @livewireScripts
+</div>
