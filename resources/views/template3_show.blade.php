04fdd973a9037461fab30f49960007eefceb771e
65160344 <65160344@go.buu.ac.th>
2024-04-07 15:25:37 +0700

    ห

--- resources/views/template3_show.blade.php
+++ resources/views/template3_show.blade.php
@@ -8,7 +8,7 @@
 </head>
     <table>
         <body>
-        <tr>
+            <tr>
                 <td colspan="2">
                     <h1> <!-- Move h1 inside td and colspan to span both columns -->
                         Template 3
@@ -17,76 +17,23 @@
             </tr>
             <td>
                 <div class="box1">
-            
+                <img class="img1" src="https://img.salehere.co.th/p/1200x0/2021/06/08/nbo0zdzxp6rd.jpg" alt="Image 1" >
                 </div>
             </td>
-        <td >
-        <div class="box2">
-            <div id="drop-area2" method="POST" enctype="multipart/form-data">
-                @csrf
-                <form>
-                <label for="input-file2">
-                    <input type="file" name="upload-image" accept="image/*" id="input-file2" hidden>
-                    <div id="img-view2">
-                    <img class="icon2" src="{{url('assets/img/system/img_project_icon.png')}}" alt="" style="width: 200px;">
-                    </div> 
-                </label>
+            <td >
+            <div class="box2">
+            <img class="img2" src="https://i1.sndcdn.com/artworks-rZkfgJHbQ8FVyD9q-1gLQdQ-t500x500.jpg" alt="Image 1" >
             </div>
             </form>
-        
-            <textarea class="detail" type="text" placeholder="Detail Project" ></textarea>
-</td>
+            <div class="detail" >
+                หมาล
+            </div>
+            </td>
             
     
 
-    <script>
-        const dropArea1 = document.getElementById("drop-area1");
-        const inputFile1 = document.getElementById("input-file1");
-        const imageView1 = document.getElementById("img-view1");
-
-        inputFile1.addEventListener("change", uploadImage1);
-
-        function uploadImage1(){
-            let imgLink1 = URL.createObjectURL(inputFile1.files[0]);
-            imageView1.style.backgroundImage = `url(${imgLink1})`;
-            imageView1.textContent="";
-            imageView1.Style.border="0";
-        }
-
-        dropArea1.addEventListener("dragover", function(e){
-            e.preventDefault();
-        });
-
-        dropArea1.addEventListener("drop", function(e){
-            e.preventDefault();
-            inputFile1.files = e.dataTransfer.files;
-            uploadImage1();
-        });
-
-        const dropArea2 = document.getElementById("drop-area2");
-        const inputFile2 = document.getElementById("input-file2");
-        const imageView2 = document.getElementById("img-view2");
-
-        inputFile2.addEventListener("change", uploadImage2);
-
-        function uploadImage2(){
-            let imgLink2 = URL.createObjectURL(inputFile2.files[0]);
-            imageView2.style.backgroundImage = `url(${imgLink2})`;
-            imageView2.textContent="";
-            imageView2.Style.border="0";
-        }
-
-        dropArea2.addEventListener("dragover", function(e){
-            e.preventDefault();
-        });
-
-        dropArea2.addEventListener("drop", function(e){
-            e.preventDefault();
-            inputFile2.files = e.dataTransfer.files;
-            uploadImage2();
-        });
-    </script>
-    </div>
-    </body>
+    
+       
+        </body>
     </table>
 @endsection
