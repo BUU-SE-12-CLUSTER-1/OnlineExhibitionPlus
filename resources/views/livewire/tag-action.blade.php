b64c19237d21ebb678a4085f48a8ff2ee26eb38d
65160219 <152172067+65160219@users.noreply.github.com>
2024-04-07 16:48:34 +0700

    update delete alert tag and company

--- resources/views/livewire/tag-action.blade.php
+++ resources/views/livewire/tag-action.blade.php
@@ -1,7 +1,8 @@
 <div>
-    <div style="margin-left:20px">
-    <a href="{{ url('/delete-tag/' . $tag->tag_id) }}"><i class="fa-solid fa-trash-can"></i></a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <a href="#" x-data x-on:click="$dispatch('open-modal', { name: '{{ $tag->tag_id }}' })"<i class="fa-solid fa-pen-to-square"></i></a>
+    <div>
+        <div style="margin-left: 30px">
+        <a href="#" x-data x-on:click="$dispatch('open-oe-alert', { name: 'delete-{{ $tag->tag_id }}' })"><i class="fa-solid fa-trash-can"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="#" x-data x-on:click="$dispatch('open-modal', { name: '{{ $tag->tag_id }}' })"><i class="fa-solid fa-pen-to-square"></i></a>
     </div>
 
     <x-modal name="{{ $tag->tag_id }}" title="Edit Tag" id="edit-box">
@@ -9,4 +10,12 @@
             @livewire('edit-tag', ['tag' => $tag])
         </x-slot>
     </x-modal>
+    <x-oe-alert name="delete-{{ $tag->tag_id }}">
+        <x-slot:body>
+        @livewire('delete-alert', ['topic'=>'tag', 'model' => $tag])
+        </x-slot>
+      </x-oe-alert>
 </div>
+
+
+
