f4ee5812c52535213b57e99ca7b6cf7ea1bf2d3c
65160219 <152172067+65160219@users.noreply.github.com>
2024-04-07 10:43:44 +0700

    update code company-list

--- resources/views/livewire/company-action.blade.php
+++ resources/views/livewire/company-action.blade.php
@@ -1,7 +1,7 @@
 <div>
-    <a href="#" x-data x-on:click="$dispatch('open-modal', { name: '{{ $company->company_id }}' })">Edit</a>
-    <a href="{{ url('/delete-company/' . $company->company_id) }}"><i class="fa-solid fa-trash-can"></i></a>
-
+    <div style="margin-left: 30px">
+    <a href="{{ url('/delete-company/' . $company->company_id) }}"><i class="fa-solid fa-trash-can"></i></a>&nbsp;&nbsp;
+    <a href="#" x-data x-on:click="$dispatch('open-modal', { name: '{{ $company->company_id }}' })"><i class="fa-solid fa-pen-to-square"></i></a></div>
     <x-modal name="{{ $company->company_id }}" title="Edit Company" id="edit-box">
         <x-slot name="body">
             @livewire('edit-company', ['company' => $company])
--- resources/views/livewire/company-list.blade.php
+++ resources/views/livewire/company-list.blade.php
@@ -2,8 +2,8 @@
 <div>
     @livewireStyles
     <div class="search-container" style="position:relative;margin-left:1370px;margin-top:-50px;">
-        <input wire:model.live="search" style="padding-left: 10px;" type="text" class="search" name="search_user" placeholder="Search Company">
-        <button type="button" class="search-button" name="btn_search_user">
+        <input wire:model.live="search" style="padding-left: 10px;" type="text" class="search" name="search_company" placeholder="Search Company">
+        <button type="button" class="search-button" name="btn_search_company">
             <i class="fa-solid fa-magnifying-glass"></i>
         </button>
     </div></br>
