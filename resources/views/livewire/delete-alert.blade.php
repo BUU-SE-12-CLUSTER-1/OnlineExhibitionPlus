87b71d8db18ee2c794e9a59ebe0f6f5d9054e4ee
65160219 <152172067+65160219@users.noreply.github.com>
2024-04-07 16:13:27 +0700

    update delete alert company

--- resources/views/livewire/delete-alert.blade.php
+++ resources/views/livewire/delete-alert.blade.php
@@ -1,11 +1,7 @@
 <div>
     @livewireStyles
     <div><i class="fa-solid fa-circle-exclamation" style="font-size: 50px;margin-left:160px; padding-bottom: 20px; color: #4E6CEF; margin-top:-20px;
-        background: -webkit-linear-gradient(91deg,#9DBEFF 42.58%, #FED9FF);
-
-;
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;">
+        background: -webkit-linear-gradient(91deg,#9DBEFF 42.58%, #FED9FF);-webkit-background-clip: text;-webkit-text-fill-color: transparent;">
         </i></div>
     <div>
         <label style="margin-left: 50px; margin-bottom:10px">Are you sure to delete this {{$topic}}?</label>
