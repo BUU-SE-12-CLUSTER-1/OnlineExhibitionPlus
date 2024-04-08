a7e0f737d0b4a93eb509a66caa25f33dcb64c5ca
65160219 <152172067+65160219@users.noreply.github.com>
2024-04-08 10:19:47 +0700

    update position icon alert and position button edit company tag advisor

--- resources/views/components/oe-alert.blade.php
+++ resources/views/components/oe-alert.blade.php
@@ -8,6 +8,9 @@
     <!-- CSS -->
     <div x-on:click="show = false" class="fixed inset-0 bg-gray-300 opacity-40"></div>
     <div class="backgroundWhite" style="padding-left: 30px; padding-right: 30px">
+        <div><i class="fa-solid fa-circle-exclamation" style="font-size: 55px;margin-left:42%; padding-bottom: 26px; color: #4E6CEF; margin-top:30px;
+            background: -webkit-linear-gradient(91deg,#9DBEFF 42.58%, #FED9FF);-webkit-background-clip: text;-webkit-text-fill-color: transparent;">
+            </i></div>
         <div class="bodyAlert">
             {{ $body }}
         </div>
--- resources/views/livewire/delete-alert.blade.php
+++ resources/views/livewire/delete-alert.blade.php
@@ -1,8 +1,5 @@
 <div>
     @livewireStyles
-    <div><i class="fa-solid fa-circle-exclamation" style="font-size: 55px;margin-left:125px; padding-bottom: 26px; color: #4E6CEF; margin-top:30px;
-        background: -webkit-linear-gradient(91deg,#9DBEFF 42.58%, #FED9FF);-webkit-background-clip: text;-webkit-text-fill-color: transparent;">
-        </i></div>
     <div style="align-items:center; justify-content:center; text-align:center">
         <label style=" margin-bottom:10px; font-size:18px" >Are you sure to delete this {{$topic}}?</label>
     </div>
