5b54388407cd6ba2154d4ec2a13615504adaff40
65160219 <152172067+65160219@users.noreply.github.com>
2024-04-08 13:41:19 +0700

    update log out alert

--- resources/views/livewire/logout-alert.blade.php
+++ resources/views/livewire/logout-alert.blade.php
@@ -1,8 +1,9 @@
 <div>
     @livewireStyles
     <div style="align-items:center; justify-content:center; text-align:center">
-        <label style=" margin-bottom:10px; font-size:18px" >Are you sure to logout?</label>
+        <label style=" margin-bottom:10px; font-size:18px" >Are you want to logout?</label>
     </div>
     <button class="buttonClear"  x-data x-on:click="$dispatch('close-oe-alert')" style="margin-left: 1px ; margin-top: 20px">Cancel</button>
-    <button class="buttonAdd" style="margin-left:112px;"><a href="{{ url('/403forbidden/') }}" style="color:inherit">Log out</a></button>
+        <button class="buttonAdd" style="margin-left:112px" onclick="window.location= '{{url('/403forbidden')}}'">Log out</button>
+
 </div>
