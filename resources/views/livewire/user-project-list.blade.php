06acfbcb3321d276fbd7b064303fc4364e72fee4
ApiratiKTBuu <65160365@go.buu.ac.th>
2024-04-09 13:59:44 +0700

    user project backend

--- resources/views/livewire/user-project-list.blade.php
+++ resources/views/livewire/user-project-list.blade.php
@@ -2,20 +2,8 @@
     @livewireStyles
     <link rel="stylesheet" href="{{ asset('assets/css/project_list.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/modal.css') }}">
-
-    <div class="search-container" style="position:relative;margin-left:990px;margin-top:-50px;">
-        <input wire:model.live="search" style="padding-left: 10px;" type="text" class="search" name="search_project" placeholder="Search Project">
-        <button type="button" class="search-button" name="btn_search_project">
-            <i class="fa-solid fa-magnifying-glass"></i>
-        </button>
-    </div><br>
             <table>
                 <tr>
-                <div class="titleBlue" style="width: 1450px">
-                    <div >
-                        <h1 id="Manageproject">Manage Project</h1>
-                    </div>
-                </div>
             </tr>
                 <?php
                 $td_count = 1;
