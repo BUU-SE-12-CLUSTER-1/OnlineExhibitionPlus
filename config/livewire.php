7afa0e6c79fd457f0dcfd67b5a2aab54a86ebe04
ApiratiKTBuu <65160365@go.buu.ac.th>
2024-04-09 10:35:00 +0700

    Update livewire.php

--- config/livewire.php
+++ config/livewire.php
@@ -156,5 +156,5 @@ return [
     */
 
     'pagination_theme' => 'tailwind',
-    'asset_url' => '/cluster1/public/assets/composer/vendor/livewire',
+    'asset_url' => '/cluster1/public/assets/composer/vendor/livewire/livewire/dist/livewire.js',
 ];
