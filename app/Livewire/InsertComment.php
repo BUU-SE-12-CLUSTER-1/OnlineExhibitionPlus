5dadf5dfa094bd91e68729983049ee0189df978b
65160219 <152172067+65160219@users.noreply.github.com>
2024-04-09 16:39:40 +0700

    update comment but fail

--- app/Livewire/InsertComment.php
+++ app/Livewire/InsertComment.php
@@ -3,9 +3,28 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\CommentModel;
 
 class InsertComment extends Component
 {
+    public $text='' ;
+    public $project_id='';
+    public $user_id='';
+    public function mount($user_id, $project_id){
+
+    }
+    public function insertComment(){
+        $this->validate([
+            'text'=>'required',
+        ]);
+        CommentModel::create([
+            'comment_text'=>$this->text,
+            'comment_proj_id'=>$this->project_id,
+            'comment_user_id'=>$this->user_id,
+        ]);
+        $this->text = '';
+
+    }
     public function render()
     {
         return view('livewire.insert-comment');
--- resources/views/livewire/insert-comment.blade.php
+++ resources/views/livewire/insert-comment.blade.php
@@ -1,3 +1,25 @@
 <div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+    @livewireStyles
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="{{ asset('assets/css/form_input.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/detail.css') }}">
+    <form wire:submit.prevent="insertComment" action="">
+        @csrf
+        <div>
+            <h1>Comment</h1>
+            <div class="comment_container">
+                <input class="comment_project" wire:model="text" type="text" name="comment"
+                    placeholder="Comment project here">
+                <br>
+                <button type="submit" class="comment_button" name="btn_comment_project">
+                    <i class="fa-solid fa-paper-plane"></i>
+                </button>
+                @error('text')
+                    <span class="oe-error" style="color: red">{{ $message }}</span>
+                @enderror
+                </br>
+            </div>
+        </div>
+    </form>
+    @livewireScripts
 </div>
