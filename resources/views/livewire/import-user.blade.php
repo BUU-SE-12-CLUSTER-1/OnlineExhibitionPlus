b4c4bf917e670f4aa357ba3f2130cad702629962
warot020210 <65160235@go.buu.ac.th>
2024-04-09 15:51:07 +0700

    update

--- resources/views/livewire/import-user.blade.php
+++ resources/views/livewire/import-user.blade.php
@@ -10,7 +10,7 @@
 <input class="oe-choose-file" wire:model="file" type="file" name="file" accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" >
 
 @error('file')
-<span class="oe-error" style="white-space:nowrap;" >{{ $message }}</span>
+<span class="oe-file-error " style="white-space:nowrap;" >{{ $message }}</span>
 @enderror
 
 <input class="buttonAdd" type="submit" value="Upload" name="btn_submit" style="position:fixed; bottom: 20px; right: 30px;  ">
